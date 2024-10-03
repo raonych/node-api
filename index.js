f7c30af5c4bbb5fd971931858d3d750bfdb60c24
raonych <raonych@gmail.com>
2024-10-03 20:02:25 -0300

    users

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express")//para usar o express
 const server = express()// para criar um servidor
 const categorias = require("./src/categorias.json")
+const users = require(".src/users.json")
 server.get("/", (req, res) =>{
     return res.json({mensagem:"salve"})
 })
 
 server.get("/categorias", (req, res) =>{
     return res.json(categorias)
+})
+server.get("/users", (req, res) =>{
+    return res.json(users)
 })
 
 server.listen(1910, () => {
