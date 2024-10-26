6085e54b1506c436c04b8c6de0d1b1fcbd82a2b5
raonych <raonych@gmail.com>
2024-10-25 21:29:40 -0300

    delete & update

--- main.js
+++ main.js
@@ -85,7 +85,7 @@ rotas.get("/", function(req, res){
     res.send("Rota Principal");
 });
 
-rotas.get("/login/:user_nome/:user_email/:user_senha", async function(req, res){
+rotas.get("/cadastrar/:user_nome/:user_email/:user_senha", async function(req, res){
     const {user_nome,user_email,user_senha} = req.params; //Guarda os parametros em variaveis
 
     const novoUsuario = await Usuario.create({user_nome,user_email,user_senha});// insert
@@ -95,7 +95,47 @@ rotas.get("/login/:user_nome/:user_email/:user_senha", async function(req, res){
         usuario: novoUsuario
     })
 });
+rotas.get("/exibir", async function (req, res) {
+    const usuarios = await Usuario.findAll(); // Busca todos os registros
+    res.json(usuarios); // Retorna os registros em formato JSON
+  });
 
+
+
+rotas.get("/editar/:id/:user_nome/:user_email", async function (req, res) {
+    const { id, user_nome, user_email } = req.params;
+    const idNumber = parseInt(id, 10); // Converte o ID para número
+  
+    const [updated] = await Usuario.update(
+      { user_nome, user_email },
+      {
+        where: { id: idNumber }, // Usa o ID numérico
+      }
+    );
+  
+    res.json({
+      mensagem: "Usuario atualizado com sucesso",
+    });
+  });
+  
+
+
+
+  // Deletar aluno via ID
+  rotas.get("/deletar/:id", async function (req, res) {
+    const { id } = req.params;
+    const idNumber = parseInt(id, 10); // Converte o ID para número
+  
+    const deleted = await Usuario.destroy({
+      where: { id: idNumber },
+    });
+  
+    if (deleted) {
+      res.json({ mensagem: "Usuario deletado com sucesso" });
+    } else {
+      res.status(404).json({ mensagem: "Usuario não encontrado" });
+    }
+  });
 
 
 rotas.listen(3001, function () {
